56a3dc17d64016e8bfc734cedda68b0fecfca9af
harinivaskumar <harinivaskumar@live.com>
2016-12-14 08:37:26 +0530

    Code Implemented
    Code added

--- app/src/main/java/com/hari/aag/contactslist/ContactsListActivity.java
+++ app/src/main/java/com/hari/aag/contactslist/ContactsListActivity.java
@@ -1,8 +1,16 @@
 package com.hari.aag.contactslist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.hari.aag.contactslist.adapter.ContactsAdapter;
 import com.hari.aag.contactslist.data.Contact;
@@ -27,6 +35,46 @@ public class ContactsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_list);
 
-        ((ListView)findViewById(R.id.id_list_contacts)).setAdapter(new ContactsAdapter(this, contacts));
+        ListView contactsListView = (ListView)findViewById(R.id.id_list_contacts);
+        contactsListView.setAdapter(new ContactsAdapter(this, contacts));
+        registerForContextMenu(contactsListView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.contact_options_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Intent intent;
+        AdapterView.AdapterContextMenuInfo info =
+                (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()){
+            case R.id.id_call:
+                intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + contacts[info.position].getPhoneNumber()));
+                Toast.makeText(this, "Making Call to " + contacts[info.position].getName() + " ...",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.id_sms:
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("sms:" + contacts[info.position].getPhoneNumber()));
+                Toast.makeText(this, "Sending Sms to " + contacts[info.position].getName() + " ...",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+        try {
+            startActivity(intent);
+        } catch (SecurityException e){
+            e.printStackTrace();
+        }
+        return true;
     }
 }
